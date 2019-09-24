46ed811874fc72fa7105aa0733fab3a33d3d5ed7
Kenneth Ofosu <kofosu89@gmail.com>
2019-09-24 01:18:15 -0400

    create Conversation model(join table)

---
+++ db/models/Conversation.js
@@ -0,0 +1,10 @@
+const conn = require('../conn');
+
+const { Sequelize } = conn;
+const { Op } = Sequelize;
+
+const Conversation = conn.define('conversation', {
+
+});
+
+module.exports = Conversation;
